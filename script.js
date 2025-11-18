ec8ccafcef739a86bd21b6d49ff68e586b504b11
Gustavo Silva <150408960+GusDev240703@users.noreply.github.com>
2025-11-18 13:57:16 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -14,5 +14,5 @@ function salvarNome() {
   }
   localStorage.setItem("nomeConvidado", nome);
   // Redireciona para a página da lista de presentes
-  window.location.href = "lista-presentes.html"; // ou o nome do seu arquivo
+  window.location.href = "produtos.html"; 
 }
